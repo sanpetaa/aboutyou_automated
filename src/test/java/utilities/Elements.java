f51b56c5a6737d7d84a699d119cadb8ff50f0ea5
sanpetaa <peter.santa.careers@gmail.com>
2022-09-24 15:12:41 +0200

    new constant waiting times

--- src/test/java/utilities/Elements.java
+++ src/test/java/utilities/Elements.java
@@ -9,24 +9,24 @@ public class Elements extends BaseTest {
 
 	public static void premium() throws InterruptedException {
 		driver.findElement(By.xpath(loc.getProperty("premium"))).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 	}
 
 	public static void poloingek() throws InterruptedException {
 		driver.findElement(By.xpath(loc.getProperty("poloingek"))).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 	}
 
 	public static void meret() throws InterruptedException {
 		driver.findElement(By.xpath(loc.getProperty("meret"))).click();
 		driver.findElement(By.xpath(loc.getProperty("M"))).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 	}
 
 	public static void rendezes() throws InterruptedException {
 		driver.findElement(By.xpath(loc.getProperty("rendezes"))).click();
 		driver.findElement(By.xpath(loc.getProperty("legalacsonyabb_ar"))).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 	}
 
 	public static void scrollDown() throws InterruptedException {
